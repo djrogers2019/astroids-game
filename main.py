10344cf6ca97d20f5b411bb3a829950ccdff2e77
Josh <rogersjdouglas@gmail.com>
2025-01-24 14:59:12 -0600

    D: Added functions to the game that enable a player sprite and allow for basic movement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -9,13 +10,26 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fps_clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers(updatable,drawable)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        player.update(dt)
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
+        dt = fps_clock.tick(60) / 1000
+
 
 
 
